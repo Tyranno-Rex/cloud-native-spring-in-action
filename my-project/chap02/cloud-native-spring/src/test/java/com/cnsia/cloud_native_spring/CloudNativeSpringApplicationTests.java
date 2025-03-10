eadfe96dd8243d76c28b764d9d6854b058bce333
Tyranno-Rex <jsilvercastle@gmail.com>
2025-03-10 20:06:36 +0900

    2강 실습 진행

---
+++ my-project/chap02/cloud-native-spring/src/test/java/com/cnsia/cloud_native_spring/CloudNativeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnsia.cloud_native_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
